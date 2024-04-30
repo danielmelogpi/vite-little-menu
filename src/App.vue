86ecb287ba58116c3870bb2827386a7e45c4716f
danielmelogpi <danielmelogpi@gmail.com>
2024-04-30 12:21:15 -0300

    Fix header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="absolute top-0 left-0 z-0">
-    <h1>Menu</h1>
-    <ul>
-      <li>Good morning</li>
-      <li>Bom dia</li>
-      <li>Bongiorno</li>
-      <li>Buenos dias</li>
-      <li>Bonjour</li>
-    </ul>
-  </div>
-  <div class="absolute top-0 z-0 bg-slate-500 w-full h-screen text-left transition-all ease-in-out" :class="{'left-0': !menuOpen, 'left-3/4': menuOpen}">
-    <button @click="menuOpen = !menuOpen">Switch collapse</button>
-  </div>
+    <div class="fixed top-0 left-0">
+      <h1>Menu</h1>
+      <ul>
+        <li>Good morning</li>
+        <li>Bom dia</li>
+        <li>Bongiorno</li>
+        <li>Buenos dias</li>
+        <li>Bonjour</li>
+      </ul>
+    </div>
+    <div class="fixed z-10 top-0 left-0 bg-green-300 w-full h-12 text-left transition-all ease-in-out" 
+    :class="{'translate-x-0': !menuOpen, 'translate-x-3/4': menuOpen}">
+        <button @click="menuOpen = !menuOpen">Open/Close</button>
+        Fxed scroll header
+    </div>
+    <div class="absolute top-12 left-0 bg-slate-500 w-full min-h-screen text-left transition-all ease-in-out" 
+    :class="{'translate-x-0': !menuOpen, 'translate-x-3/4': menuOpen}">
+      <div> {{ new Array(500).fill('Lorem ipsum').join(' ') }}</div>
+    </div>
+  
+ 
 </template>
 
 <script setup lang="ts">
@@ -19,4 +27,8 @@ import { ref } from 'vue';
 
 const menuOpen = ref(false)
 </script>
-<style scoped></style>
+<style scoped>
+body {
+  overflow-x: hidden;
+}
+</style>
